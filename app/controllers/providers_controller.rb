5b2199aad14bdd853247f0c84165c61cb20aea9e
Jaime Luong <jaime.luong@gmail.com>
2023-02-02 14:08:28 -0600

    Fill out Providers controller, update routes

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,2 +1,37 @@
 class ProvidersController < ApplicationController
+    def index
+        @providers = Provider.all
+        render json: @providers
+    end
+
+    def show
+        @provider = Provider.find(params[:id])
+        render json: @provider
+    end
+
+    def create
+        @provider = Provider.create(
+            first_name: params[:first_name],
+            last_name: params[:last_name],
+            position: params[:position]
+        )
+        render json: @provider
+    end
+
+    def update
+        @provider = Provider.find(params[:id])
+        @provider.update(
+            first_name: params[:first_name],
+            last_name: params[:last_name],
+            position: params[:position]
+        )
+        render json: @provider
+    end
+
+    def destroy
+        @providers = Provider.all
+        @provider = Provider.find(params[:id])
+        @provider.destroy
+        render json: @providers
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :providers
+  resources :providers, only [:index, :show, :create, :update, :destroy]
 end
